0af9318c7b2c181130288f952489161f570c9a16
boomer934 <lupomarino09@gmail.com>
2025-08-28 17:27:50 +0200

    modifica del layout

--- client/src/components/FormRegister.jsx
+++ client/src/components/FormRegister.jsx
@@ -10,25 +10,27 @@ export default function FormRegister({ user, setUser }) {
 
   return (
     <>
-    <div className=' flex flex-col items-center justify-center h-dvh'>
-      <form onSubmit={handleSubmit} className='flex items-center justify-center flex-col h-auto rounded-3xl p-6 pt-0'>
-        <label htmlFor="name" className=' self-start'>nome:</label>
+    
+    <div className=' flex flex-col items-center h-dvh mt-8'>
+      <h1 className=' text-[20px] p-2.5'>Sign Up For free</h1>
+      <form onSubmit={handleSubmit} className='w-full flex items-start justify-center flex-col h-auto rounded-3xl p-6 pt-0'>
+        <label htmlFor="name" className=' self-start'>Nome</label>
         <input 
         type="text"
         name="name"
         value={user.name}
         onChange={(e)=>setUser({...user,name:e.target.value})} 
         placeholder='Inserisci il tuo nome...'
-        className='focus:outline-0 p-1'/>
-        <label htmlFor="surname" className=' self-start'>cognome:</label>
+        className='focus:outline-0 p-1 my-2 outline-1 outline-gray-500 rounded-3xl w-full'/>
+        <label htmlFor="surname" className=' self-start'>Cognome</label>
         <input 
         type="text"
         name="surname"
         value={user.surname}
         onChange={(e)=>setUser({...user,surname:e.target.value})} 
         placeholder='Inserisci il tuo cognome...'
-        className='focus:outline-0 p-1'/>
-        <label htmlFor="email" className=' self-start'>email:</label>
+        className='focus:outline-0 p-1 my-2 outline-1 outline-gray-500 rounded-3xl w-full'/>
+        <label htmlFor="email" className=' self-start'>Email</label>
         <input 
           type="email" 
           name="email" 
@@ -36,9 +38,9 @@ export default function FormRegister({ user, setUser }) {
           value={user.email}
           onChange={(e) => setUser({ ...user, email: e.target.value })}
           placeholder="Inserisci la tua email.." 
-          className='focus:outline-0 p-1'
+          className='focus:outline-0 p-1 my-2 outline-1 outline-gray-500 rounded-3xl w-full'
         />
-        <label htmlFor="password" className=' self-start'>password:</label>
+        <label htmlFor="password" className=' self-start'>Password</label>
         <input 
           type="password" 
           name="password" 
@@ -46,12 +48,12 @@ export default function FormRegister({ user, setUser }) {
           value={user.password}
           onChange={(e) => setUser({ ...user, password: e.target.value })}
           placeholder="Inserisci la tua password.."
-          className='focus:outline-0 p-1' 
+          className='focus:outline-0 p-1 outline-1 outline-gray-500 rounded-3xl w-full' 
         />
         <input 
           type="submit" 
           value="Invia" 
-          className="bg-amber-500 w-20 h-8 cursor-pointer rounded-md text-white font-semibold" 
+          className="bg-amber-500 w-20 h-8 cursor-pointer rounded-md text-white font-semibold mt-3 self-center focus:scale-110" 
         />
       </form>
     </div>
