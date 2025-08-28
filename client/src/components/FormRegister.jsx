6409805f9f9cbae65a34a95ec1a9126981a322c3
boomer934 <lupomarino09@gmail.com>
2025-08-28 18:00:48 +0200

    Personalizzazione e refattorizzazione di windsurf

--- client/src/components/FormRegister.jsx
+++ client/src/components/FormRegister.jsx
@@ -1,63 +1,71 @@
 import axios from 'axios'
-export default function FormRegister({ user, setUser }) {
-  const handleSubmit = async(e) => {
-    e.preventDefault()
-    axios.post("http://localhost:5000/register",user)
-    .then(res=>res.data)
-    setUser({name:"",surname:"",email:"",password:""})
-  }
-  
+export default function RegisterForm({ user, setUser }) {
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post("http://localhost:5000/register", user);
+      setUser({ name: "", surname: "", email: "", password: "" });
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
-    <>
-    
-    <div className=' flex flex-col items-center h-dvh mt-8'>
-      <h1 className=' text-[20px] p-2.5'>Sign Up For free</h1>
-      <form onSubmit={handleSubmit} className='w-full flex items-start justify-center flex-col h-auto rounded-3xl p-6 pt-0'>
-        <label htmlFor="name" className=' self-start'>Nome</label>
-        <input 
-        type="text"
-        name="name"
-        value={user.name}
-        onChange={(e)=>setUser({...user,name:e.target.value})} 
-        placeholder='Inserisci il tuo nome...'
-        className='focus:outline-0 p-1 my-2 outline-1 outline-gray-500 rounded-3xl w-full'/>
-        <label htmlFor="surname" className=' self-start'>Cognome</label>
-        <input 
-        type="text"
-        name="surname"
-        value={user.surname}
-        onChange={(e)=>setUser({...user,surname:e.target.value})} 
-        placeholder='Inserisci il tuo cognome...'
-        className='focus:outline-0 p-1 my-2 outline-1 outline-gray-500 rounded-3xl w-full'/>
-        <label htmlFor="email" className=' self-start'>Email</label>
-        <input 
-          type="email" 
-          name="email" 
-          id="emailUtente"
+    <div className="flex flex-col items-center h-full mt-8">
+      <h1 className="text-4xl p-2.5">Sign Up For free</h1>
+      <form onSubmit={handleSubmit} className="w-full flex items-start justify-center flex-col h-auto rounded-3xl p-6 pt-0 ">
+        <label htmlFor="name" className="self-start">
+          Name
+        </label>
+        <input
+          type="text"
+          name="name"
+          value={user.name}
+          onChange={({ target }) => setUser({ ...user, name: target.value })}
+          placeholder="Enter your name..."
+          className="focus:outline-0 p-1 my-2 outline-1 outline-gray-500 rounded-3xl w-full"
+        />
+        <label htmlFor="surname" className="self-start">
+          Surname
+        </label>
+        <input
+          type="text"
+          name="surname"
+          value={user.surname}
+          onChange={({ target }) => setUser({ ...user, surname: target.value })}
+          placeholder="Enter your surname..."
+          className="focus:outline-0 p-1 my-2 outline-1 outline-gray-500 rounded-3xl w-full"
+        />
+        <label htmlFor="email" className="self-start">
+          Email
+        </label>
+        <input
+          type="email"
+          name="email"
+          id="email"
           value={user.email}
-          onChange={(e) => setUser({ ...user, email: e.target.value })}
-          placeholder="Inserisci la tua email.." 
-          className='focus:outline-0 p-1 my-2 outline-1 outline-gray-500 rounded-3xl w-full'
+          onChange={({ target }) => setUser({ ...user, email: target.value })}
+          placeholder="Enter your email..."
+          className="focus:outline-0 p-1 my-2 outline-1 outline-gray-500 rounded-3xl w-full"
         />
-        <label htmlFor="password" className=' self-start'>Password</label>
-        <input 
-          type="password" 
-          name="password" 
-          id="userPassword"
+        <label htmlFor="password" className="self-start">
+          Password
+        </label>
+        <input
+          type="password"
+          name="password"
+          id="password"
           value={user.password}
-          onChange={(e) => setUser({ ...user, password: e.target.value })}
-          placeholder="Inserisci la tua password.."
-          className='focus:outline-0 p-1 outline-1 outline-gray-500 rounded-3xl w-full' 
+          onChange={({ target }) => setUser({ ...user, password: target.value })}
+          placeholder="Enter your password..."
+          className="focus:outline-0 p-1 outline-1 outline-gray-500 rounded-3xl w-full"
         />
-        <input 
-          type="submit" 
-          value="Invia" 
-          className="bg-amber-500 w-20 h-8 cursor-pointer rounded-md text-white font-semibold mt-3 self-center focus:scale-110" 
+        <input
+          type="submit"
+          value="Sign Up"
+          className="bg-red-500 w-full h-8 cursor-pointer text-white font-semibold mt-5 self-center rounded-3xl focus:scale-110 focus:bg-red-600 focus:transition duration-300 ease-in-out"
         />
       </form>
     </div>
-      
-    </>
-  )
+  );
 }
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -5,13 +5,10 @@ export default function Navbar(){
             <nav className="bg-gray-900 text-white shadow-md">
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div className="flex justify-between h-16 items-center">
-                    
                     {/* Logo / Titolo */}
                     <div className="flex-shrink-0">
                         <h1 className="text-2xl font-bold tracking-wide text-red-500">WATCHLIST</h1>
                     </div>
-
-
                     </div>
                 </div>
             </nav>
