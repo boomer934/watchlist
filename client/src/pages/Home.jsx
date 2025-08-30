9372ce04c111a51842acf1d7d4140c85319771de
boomer934 <lupomarino09@gmail.com>
2025-08-30 16:09:55 +0200

    Aggiunta visualizzazione dei film piu popolari nella homepage in modo da non rimanere vuota

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,12 +3,26 @@ import { useEffect, useState } from "react"
 import Navbar from "../components/NavBar"
 import SearchBar from "../components/SearchBar"
 import Card from "../components/Card"
+import axios from "axios"
 export default function Home(){
     const navigate = useNavigate()
     const [user,setUser] = useState({name:"",surname:"",email:"",password:""})
     const [movieTitle,setMovieTitle] = useState("")
     const [movies,setMovies] = useState([{}])
     
+    useEffect(()=>{
+        try {
+            axios.get("https://api.themoviedb.org/3/movie/popular?language=en",{
+                params: { api_key: "ae7e3d3ba153dd817538a94cd60ac92e" },
+                headers: { Authorization: `Bearer ${localStorage.getItem("token")}` }
+            })
+            .then(res=>setMovies(res.data.results))
+            .catch(err=>console.log(err))
+        } catch (error){
+            console.log(error)
+        }
+        
+    },[])
     return(
         <>
             <Navbar user={user} setUser={setUser}></Navbar>
