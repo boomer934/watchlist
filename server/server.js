27634eb04b59462857eb50ab94b97c754c1a598a
boomer934 <lupomarino09@gmail.com>
2025-09-02 12:51:26 +0200

    Add initial HTML and SVG assets for Vite + React setup

--- server/server.js
+++ server/server.js
@@ -1,21 +1,31 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const router_post = require('./Router/post')
-const router_get = require('./Router/get')
-const router_delete = require('./Router/delete')
-const router_put = require('./Router/put')
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const router_post = require('./Router/post');
+const router_get = require('./Router/get');
+const router_delete = require('./Router/delete');
+const router_put = require('./Router/put');
+
+const { executeQuery } = require('./db'); // se usi db.js
+const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
+app.use(cors());
 
+// API Router
+app.use('/', router_post);
+app.use('/', router_get);
+app.use('/', router_delete);
+app.use('/', router_put);
 
-app.use(express.json())
-app.use(cors())
+// Servire frontend React Vite dalla cartella dist
+app.use(express.static(path.join(__dirname, 'dist')));
 
-app.use('/',router_post)
-app.use('/',router_get)
-app.use('/',router_delete)
-app.use('/',router_put)
-app.listen(port,()=>{
-    console.log(`Server is listening on port ${port}`)
-})
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
