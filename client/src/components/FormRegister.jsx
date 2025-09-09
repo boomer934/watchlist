e1d85fa6a34d23a57dc953b309be3e3dd4c9e373
boomer934 <lupomarino09@gmail.com>
2025-09-09 18:57:52 +0200

    fix: update navigation path in RegisterForm to remove base URL dependency

--- client/src/components/FormRegister.jsx
+++ client/src/components/FormRegister.jsx
@@ -9,7 +9,7 @@ export default function RegisterForm({ user, setUser }) {
     try {
       const response = await axios.post(`${import.meta.env.VITE_API_URL}`, user);
       setUser({ name: "", surname: "", email: "", password: "" });
-      navigate(`${import.meta.env.VITE_API_URL}/login`);
+      navigate(`/login`);
     } catch (error) {
       console.error(error);
     }
