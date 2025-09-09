837e71776fee604387beeee7aab5e4c62f2db4eb
boomer934 <lupomarino09@gmail.com>
2025-09-09 20:09:56 +0200

    fix: remove unnecessary semicolon in import statement in RegisterForm

--- client/src/components/FormRegister.jsx
+++ client/src/components/FormRegister.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom'
 
 export default function RegisterForm({ user, setUser }) {
   const navigate = useNavigate();
